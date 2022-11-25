78943546f9b556817e1c6432cce56ff9149c578e
Kevin <kevin.572.94@gmail.com>
2022-11-25 21:07:31 +0800

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="flex p-5 bg-gray-700">
+        <footer className="footer font-bold mx-auto text-gray-200 p-10">
+            <p>Copyright © 2022 - Kevin Broncano</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="p-4 bg-gray-800 text-gray-100">
+        <div className="container flex justify-between h-16 mx-auto my-4 md:justify-center md:space-x-8">
+           {/* <ul className="items-stretch hidden space-x-3 md:flex">
+                <li className="flex">
+                    <a rel="noopener noreferrer" href="#"
+                        className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
+                </li>
+                <li className="flex">
+                    <a rel="noopener noreferrer" href="#"
+                        className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
+                </li>
+                <li className="flex">
+                    <a rel="noopener noreferrer" href="#"
+                        className="flex items-center px-4 -mb-1 border-b-2 border-transparent text-violet-400 border-violet-400">Link</a>
+                </li>
+            </ul>  */}
+            <a href="/" rel="noopener" aria-label="Back to homepage" className="flex items-center p-2">
+                <img src="/src/assets/img/avatar-2-1.png" className="p-2 border-teal-400 border-2 animate-[pulse_8s_ease-in-out_infinite] rounded-full max-w-sm shadow-2x w-20 h-20 text-violet-400" />
+                    
+            </a>
+            {/* <ul className="items-stretch hidden space-x-3 md:flex">
+                <li className="flex">
+                    <a rel="noopener noreferrer" href="#"
+                        className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
+                </li>
+                <li className="flex">
+                    <a rel="noopener noreferrer" href="#"
+                        className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
+                </li>
+                <li className="flex">
+                    <a rel="noopener noreferrer" href="#"
+                        className="flex items-center px-4 -mb-1 border-b-2 border-transparent">Link</a>
+                </li>
+            </ul> */}
+            <button title="Button" type="button" className="p-4 md:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                    className="w-6 h-6 text-gray-100">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
+                    </path>
+                </svg>
+            </button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
